d01c953573b147f3b37c38153536af633a560c28
Antoni Bertran <abertranb@uoc.edu>
2019-12-29 09:22:17 +0100

    Fixed warning on serialized data

--- lib.php
+++ lib.php
@@ -15,55 +15,60 @@ use block_configurable_reports\upload_to_ftp_form;
  * This fragment servers the form to send reports via email.
  *
  * @param array|object $args List of named arguments for the fragment loader.
+ *
  * @return string
  */
-function block_configurable_reports_output_fragment_send_email_form($args) {
-    $args = (object) $args;
-    $context = $args->context;
-    $formdata = [];
-    if (!empty($args->jsonformdata)) {
-        $serialiseddata = json_decode($args->jsonformdata, true);
-        parse_str($serialiseddata, $formdata);
-    }
-    $formdata = (object) $formdata;
+function block_configurable_reports_output_fragment_send_email_form( $args ) {
+	$args     = (object) $args;
+	$context  = $args->context;
+	$formdata = [];
+	if ( ! empty( $args->jsonformdata ) ) {
+		$serialiseddata = json_decode( $args->jsonformdata, true );
+		parse_str( $serialiseddata, $formdata );
+	}
+	$formdata = (object) $formdata;
 
-    require_capability('block/configurable_reports:viewreports', $context);
+	require_capability( 'block/configurable_reports:viewreports', $context );
 
-    $editoroptions = send_email_form::get_editor_options($context);
-    $formdata = file_prepare_standard_editor($formdata, 'content', $editoroptions, $context, 'block_configurable_reports',
-        'send_report_email_body');
+	$editoroptions = send_email_form::get_editor_options( $context );
+	$formdata      = file_prepare_standard_editor( $formdata, 'content', $editoroptions, $context,
+		'block_configurable_reports',
+		'send_report_email_body' );
 
-    $mform = new send_email_form(null, ['context' => $context], 'post', '', null, true, (array) $formdata);
-    if (!empty($serialiseddata)) {
-        // Call validation functions and show errors if applies.
-        $mform->is_validated();
-    }
+	$mform = new send_email_form( null, [ 'context' => $context ], 'post', '', null, true, (array) $formdata );
+	if ( ! empty( $serialiseddata ) ) {
+		// Call validation functions and show errors if applies.
+		$mform->is_validated();
+	}
 
-    return $mform->render();
+	return $mform->render();
 }
 
 /**
  * This fragment servers the form to upload reports via FTP.
  *
  * @param array|object $args List of named arguments for the fragment loader.
+ *
  * @return string
  */
-function block_configurable_reports_output_fragment_upload_to_ftp_form($args) {
-    $args = (object) $args;
-    $context = $args->context;
-    $formdata = [];
-    if (!empty($args->jsonformdata)) {
-        $serialiseddata = json_decode($args->jsonformdata, true);
-        parse_str($serialiseddata, $formdata);
-    }
+function block_configurable_reports_output_fragment_upload_to_ftp_form( $args ) {
+	$args     = (object) $args;
+	$context  = $args->context;
+	$formdata = [];
+	if ( ! empty( $args->jsonformdata ) ) {
+		$serialiseddata = json_decode( $args->jsonformdata, true );
+		if ( count( $serialiseddata ) > 0 ) {
+			parse_str( $serialiseddata, $formdata );
+		}
+	}
 
-    require_capability('block/configurable_reports:viewreports', $context);
+	require_capability( 'block/configurable_reports:viewreports', $context );
 
-    $mform = new upload_to_ftp_form(null, ['context' => $context], 'post', '', null, true, $formdata);
-    if (!empty($serialiseddata)) {
-        // Call validation functions and show errors if applies.
-        $mform->is_validated();
-    }
+	$mform = new upload_to_ftp_form( null, [ 'context' => $context ], 'post', '', null, true, $formdata );
+	if ( ! empty( $serialiseddata ) ) {
+		// Call validation functions and show errors if applies.
+		$mform->is_validated();
+	}
 
-    return $mform->render();
+	return $mform->render();
 }
